3c49333c1c49d645a19f931190bcf16f87fe73d0
robingiannattasio <robin.giannattasio@gmail.com>
2021-04-07 15:50:27 -0400

    Add provider to basic layout

--- components/layouts/basic.jsx
+++ components/layouts/basic.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import CountProvider from '../../context/CountProvider';
+
 const Basic = props => {
     const [header, main, section, footer] = props.children;
   
     return (
-      <>
+      <CountProvider>
         <header>{header}</header>
         <main>{main}</main>
         <section>{section}</section>
         <footer>{footer}</footer>
-      </>
+      </CountProvider>
     )
   }
   
